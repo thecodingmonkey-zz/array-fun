2eb635f04f4141411e94d99c684be3e600e45e8a
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-16 18:20:54 -1000

    add min and max functions

--- arrayfun.js
+++ arrayfun.js
@@ -6,5 +6,19 @@ module.exports = {
     else{
       return array.slice(0, n); // return an array if n is specified (even if one element)
     }
-  }
+  },
+
+  max: function (array) {
+    return array.reduce( function(p, c) {
+      return (p < c) ? c : p;  // return greater of two numbers
+    } );
+  },
+
+  min: function (array) {
+    return array.reduce( function(p, c) {
+      return (p < c) ? p : c;  // return lesser of two numbers
+    } );
+  },
+
+
 };
