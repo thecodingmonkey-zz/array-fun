fbd611387769869c13bcd25cb907225a922168de
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-16 19:14:42 -1000

    finished everything all exercises.

--- arrayfun.js
+++ arrayfun.js
@@ -20,5 +20,55 @@ module.exports = {
     } );
   },
 
+  intersection: function () { // this function takes a variable # of parameters
+//    console.log(arguments.toString() );
+    var args = [].slice.call(arguments);   // get arguments as array
+      // http://javascript.info/tutorial/arguments
 
+    return args.reduce(
+      function(array1, array2) {
+        return array1.filter( function(val, idx, arr) {
+          return (array2.some(function(c,i,a) {return c === val;}));
+        } );
+      }
+    );
+  },
+
+  difference: function() {
+//    console.log(arguments.toString() );
+    var args = [].slice.call(arguments);   // get arguments as array
+      // http://javascript.info/tutorial/arguments
+
+    return args.reduce(
+      function(array1, array2) {
+        return array1.filter( function(val, idx, arr) {
+          return (array2.every(function(c,i,a) {return c !== val;}));
+        } );
+      }
+    );
+  },
+
+  uniq: function() {
+    //    console.log(arguments.toString() );
+    var args = [].slice.call(arguments);   // get arguments as array
+      // http://javascript.info/tutorial/arguments
+
+    var flatArray = args.reduce(
+      function(array1, array2) {
+        return array1.concat(array2);
+      }
+    );
+
+    return flatArray.filter(function(c,i,a) {
+      return (i === a.indexOf(c));
+    });
+  },
+
+  contains: function(array, target) {
+    if (arguments[2] != undefined) {
+      return false;
+    }
+
+    return array.indexOf(target) !== -1;
+  }
 };
